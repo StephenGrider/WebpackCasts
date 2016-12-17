2747ae892045db0bb39b160916c818c069ebd3b3
stephengrider <ste.grider@gmail.com>
2016-12-16 17:28:14 -0800

    webpack runs

---
+++ js_modules/src/index.js
@@ -0,0 +1,4 @@
+const sum = require('./sum');
+
+const total = sum(10, 5);
+console.log(total);
---
+++ js_modules/webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  }
+};
+
+module.exports = config;
