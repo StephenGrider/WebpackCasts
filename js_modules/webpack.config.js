c67a09b1f6eafafa1fbcf70ea1b3fddf3007e0f6
stephengrider <ste.grider@gmail.com>
2016-12-19 17:48:34 -0800

    added extract text

--- js_modules/webpack.config.js
+++ js_modules/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const config = {
   entry: './src/index.js',
@@ -13,11 +14,16 @@ const config = {
         test: /\.js$/
       },
       {
-        use: ['style-loader', 'css-loader'],
+        loader: ExtractTextPlugin.extract({
+          loader: 'css-loader'
+        }),
         test: /\.css$/
       }
     ]
-  }
+  },
+  plugins: [
+    new ExtractTextPlugin('style.css')
+  ]
 };
 
 module.exports = config;
