cd41da0109b97768c9ac3cc93a203128d748ca16
stephengrider <ste.grider@gmail.com>
2016-12-19 16:58:56 -0800

    added babelrc

--- js_modules/build/bundle.js
+++ js_modules/build/bundle.js
@@ -68,22 +68,28 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ function(module, exports) {
+/***/ function(module, exports, __webpack_require__) {
 
-const sum = (a, b) => a + b;
+"use strict";
+
+
+var sum = function sum(a, b) {
+  return a + b;
+};
 
 module.exports = sum;
-
 
 /***/ },
 /* 1 */
 /***/ function(module, exports, __webpack_require__) {
 
-const sum = __webpack_require__(0);
+"use strict";
 
-const total = sum(10, 5);
+
+var sum = __webpack_require__(0);
+
+var total = sum(10, 5);
 console.log(total);
-
 
 /***/ }
 /******/ ]);
--- js_modules/webpack.config.js
+++ js_modules/webpack.config.js
@@ -9,7 +9,8 @@ const config = {
   module: {
     rules: [
       {
-        use: 'babel-loader'
+        use: 'babel-loader',
+        test: /\.js$/
       }
     ]
   }
