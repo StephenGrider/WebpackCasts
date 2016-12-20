6bfa68ddf306b9cdf0e4505b5c8821ffda792ed1
stephengrider <ste.grider@gmail.com>
2016-12-19 18:07:57 -0800

    added image loaders

--- js_modules/src/image_viewer.js
+++ js_modules/src/image_viewer.js
@@ -1,6 +1,14 @@
-import '../styles/image_viewer.css'
+import big from '../assets/big.jpg';
+import small from '../assets/small.jpg';
+import '../styles/image_viewer.css';
 
 const image = document.createElement('img');
-image.src = 'http://lorempixel.com/400/400';
+image.src = small;
 
 document.body.appendChild(image);
+
+
+const bigImage = document.createElement('img');
+bigImage.src = big;
+
+document.body.appendChild(bigImage);
