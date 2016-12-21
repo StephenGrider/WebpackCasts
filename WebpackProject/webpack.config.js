705fb6d4d275777af9ab8629582f4dc78318c7ed
stephengrider <ste.grider@gmail.com>
2016-12-20 17:25:43 -0800

    finished initial webpack config

--- WebpackProject/webpack.config.js
+++ WebpackProject/webpack.config.js
@@ -13,6 +13,10 @@ module.exports = {
         use: 'babel-loader',
         test: /\.js$/,
         exclude: /node_modules/
+      },
+      {
+        use: ['style-loader', 'css-loader'],
+        test: /\.css$/
       }
     ]
   }
