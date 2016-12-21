3a26b6a4a331ed68460ebaad6a3f219c13e80bd0
stephengrider <ste.grider@gmail.com>
2016-12-20 16:41:06 -0800

    added button logic

--- js_modules/src/index.js
+++ js_modules/src/index.js
@@ -1,5 +1,7 @@
-import sum from './sum';
-import './image_viewer';
+const button = document.createElement('button');
+button.innerText = 'Click me';
+button.onclick = () => {
 
-const total = sum(10, 5);
-console.log(total);
+};
+
+document.body.appendChild(button);
