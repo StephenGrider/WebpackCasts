ad6da3d0d8f09b2c1538e31e8c0b8c40223dbbc2
stephengrider <ste.grider@gmail.com>
2016-12-20 16:47:08 -0800

    added module splitting

--- js_modules/src/image_viewer.js
+++ js_modules/src/image_viewer.js
@@ -1,14 +1,9 @@
-import big from '../assets/big.jpg';
 import small from '../assets/small.jpg';
 import '../styles/image_viewer.css';
 
-const image = document.createElement('img');
-image.src = small;
+export default () => {
+  const image = document.createElement('img');
+  image.src = small;
 
-document.body.appendChild(image);
-
-
-const bigImage = document.createElement('img');
-bigImage.src = big;
-
-document.body.appendChild(bigImage);
+  document.body.appendChild(image);
+};
--- js_modules/src/index.js
+++ js_modules/src/index.js
@@ -1,7 +1,9 @@
 const button = document.createElement('button');
 button.innerText = 'Click me';
 button.onclick = () => {
-
+  System.import('./image_viewer').then(module => {
+    console.log(module);
+  });
 };
 
 document.body.appendChild(button);
