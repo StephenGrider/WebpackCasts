4b7e3509b26a2e392b4c9dc2fd1474fb2f0b4fa3
stephengrider <ste.grider@gmail.com>
2016-12-27 10:36:46 -0800

    added vendor bundle

--- WebpackProject/webpack.config.js
+++ WebpackProject/webpack.config.js
@@ -1,11 +1,19 @@
 var webpack = require('webpack');
 var path = require('path');
 
+const VENDOR_LIBS = [
+  'react', 'lodash', 'redux', 'react-redux', 'react-dom',
+  'faker', 'react-input-range', 'redux-form', 'redux-thunk'
+];
+
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    bundle: './src/index.js',
+    vendor: VENDOR_LIBS
+  },
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: '[name].js'
   },
   module: {
     rules: [
