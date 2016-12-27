c612ed62820b2cb0578eccb087806c4826f84368
stephengrider <ste.grider@gmail.com>
2016-12-27 12:36:34 -0800

    finished caching

--- WebpackProject/src/index.js
+++ WebpackProject/src/index.js
@@ -8,7 +8,7 @@ import Routes from './router';
 import '../style/materialize.css';
 import '../style/react-range.css';
 import '../style/style.css';
-
+console.log('hi!');
 const App = () => {
   const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
 
--- WebpackProject/webpack.config.js
+++ WebpackProject/webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
   },
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: '[name].js'
+    filename: '[name].[chunkhash].js'
   },
   module: {
     rules: [
@@ -31,7 +31,7 @@ module.exports = {
   },
   plugins: [
     new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor'
+      names: ['vendor', 'manifest']
     }),
     new HtmlWebpackPlugin({
       template: 'src/index.html'
