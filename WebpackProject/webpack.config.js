57eb9fb068fd8bb8a0dfc225bfe315b8bfa34220
stephengrider <ste.grider@gmail.com>
2016-12-27 21:20:14 -0800

    prod tweaks

--- WebpackProject/webpack.config.js
+++ WebpackProject/webpack.config.js
@@ -30,6 +30,9 @@ module.exports = {
     ]
   },
   plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    }),
     new webpack.optimize.CommonsChunkPlugin({
       names: ['vendor', 'manifest']
     }),
