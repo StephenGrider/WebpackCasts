c2cd24009124f3ee15ff9221adbb8c964a313281
stephengrider <ste.grider@gmail.com>
2016-12-27 16:51:29 -0800

    deployment tweaks

--- WebpackProject/webpack.config.js
+++ WebpackProject/webpack.config.js
@@ -35,6 +35,9 @@ module.exports = {
     }),
     new HtmlWebpackPlugin({
       template: 'src/index.html'
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': process.env.NODE_ENV
     })
   ]
 };
