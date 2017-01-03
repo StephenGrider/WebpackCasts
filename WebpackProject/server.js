a93f9bb08fa15c9d9e5b026a0168af51c815b423
stephengrider <ste.grider@gmail.com>
2017-01-03 15:27:20 -0800

    added prod config

--- WebpackProject/server.js
+++ WebpackProject/server.js
@@ -1,10 +1,21 @@
 const express = require('express');
-const webpackMiddleware = require('webpack-dev-middleware');
-const webpack = require('webpack');
-const webpackConfig = require('./webpack.config.js');
+const path = require('path');
 
 const app = express();
 
-app.use(webpackMiddleware(webpack(webpackConfig)));
+// Server routes...
+app.get('/hello', (req, res) => res.send({ hi: 'there' }));
 
-app.listen(3050, () => console.log('Listening'));
+if (process.env.NODE_ENV !== 'production') {
+  const webpackMiddleware = require('webpack-dev-middleware');
+  const webpack = require('webpack');
+  const webpackConfig = require('./webpack.config.js');
+  app.use(webpackMiddleware(webpack(webpackConfig)));
+} else {
+  app.use(express.static('dist'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
+  });
+}
+
+app.listen(process.env.PORT || 3050, () => console.log('Listening'));
