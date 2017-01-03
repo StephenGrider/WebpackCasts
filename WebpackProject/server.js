325bab93bb3d0695326b64d92259d9ec2039c16a
stephengrider <ste.grider@gmail.com>
2017-01-03 15:00:13 -0800

    added middleware

--- WebpackProject/server.js
+++ WebpackProject/server.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const webpackMiddleware = require('webpack-dev-middleware');
+const webpack = require('webpack');
+const webpackConfig = require('./webpack.config.js');
 
 const app = express();
+
+app.use(webpackMiddleware(webpack(webpackConfig)));
 
 app.listen(3050, () => console.log('Listening'));
